b2c2f1cadf80573107d8eaeebf2b9edb7d2b1274
D4ITON <daitonmh100@gmail.com>
2018-09-09 20:50:22 -0500

    1ra parte edicion usuarios

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
@@ -46,9 +47,9 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return view('admin.users.show',compact('user'));
     }
 
     /**
@@ -57,9 +58,9 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        return view('admin.users.edit',compact('user'));
     }
 
     /**
@@ -69,9 +70,16 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => ['required',Rule::unique('users')->ignore($user->id)]
+        ]);
+
+        $user->update($data);
+
+        return back()->withFlash('Usuario actualizado');
     }
 
     /**
