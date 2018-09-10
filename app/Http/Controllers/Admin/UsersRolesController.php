cb49bbae383ae48139fe11947918c9433b95f723
D4ITON <daitonmh100@gmail.com>
2018-09-10 08:26:44 -0500

    agragar y quitar roles a usuarios

---
+++ app/Http/Controllers/Admin/UsersRolesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersRolesController extends Controller
+{
+    public function update(Request $request, User $user)
+    {
+        $user->syncRoles($request->roles);
+
+        return back()->withFlash('Los roles han sido actualizados');
+    }
+}
