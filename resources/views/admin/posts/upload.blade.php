45fa36c52289752dae7e0572a6ad4faba8e9b6c4
d4iton <daitonmh100@gmail.com>
2019-07-31 12:20:33 -0500

    agrega boton envio de fotos

--- resources/views/admin/posts/upload.blade.php
+++ resources/views/admin/posts/upload.blade.php
@@ -3,11 +3,14 @@
     <div class="box box-primary">
         <div class="box-body">
             <div class="form-group">
+                <label>Agregar imágenes</label>
                 <form action="{{route('admin.drive.create')}}" method="post" enctype="multipart/form-data">
                     @csrf
                     <input type="hidden" name="post_id" value="{{ $post_id }}">
-                    <input type="file" name="file">
-                    <input type="submit" value="Submit">
+                    {{-- <label for="file">Choose a profile picture:</label> --}}
+                    {{-- <input type="submit" class="btn btn-primary btn-block" value="Submit"> --}}
+                    {{-- <input type="file" name="file"> --}}
+                    <a href="" class="btn btn-primary btn-block">Nueva imagen</a>
                 </form>
             </div>
         </div>
